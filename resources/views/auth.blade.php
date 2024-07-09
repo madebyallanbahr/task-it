bd0356b6874416a538e09a6b6d5602d9c2a7a8d8
Made by Allan Bahr <madebyallanbahr@gmail.com>
2024-07-08 23:13:25 -0300

    refactor: refatora ui/blade da autenticação de usuários/contas

--- resources/views/auth.blade.php
+++ resources/views/auth.blade.php
@@ -1,90 +1,102 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html class="dark" lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Task.it</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-        .form-container {
-            max-width: 400px;
-            width: 100%;
-            padding: 15px;
-            background-color: #fff;
-            border-radius: 8px;
-            box-shadow: 0 0 10px rgba(0,0,0,0.1);
-        }
-    </style>
+    <title>taskit</title>
+    <link
+        rel="stylesheet"
+        href="https://unpkg.com/franken-ui-releases@0.0.13/dist/default.min.css"
+    />
 </head>
-<body class="bg-light d-flex align-items-center ">
-<div class="container">
-    <div class="row justify-content-center align-items-center">
-        <div class="col-md-6 col-lg-4">
-            <div id="login-form" class="container-fluid">
-                <form class="p-4 bg-white shadow-lg rounded form-container" method="post" action="{{route('auth.login')}}">
-                    @csrf
-                    <h1 class="h3 mb-3 fw-normal text-center">Login</h1>
-                    <p class="small text-secondary text-center">Insira as credenciais para acessar a plataforma.</p>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="loginEmail" name="email" placeholder="name@example.com" required>
-                        <label for="loginEmail">E-mail</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="loginPassword" name="password" placeholder="Password" required>
-                        <label for="loginPassword">Senha</label>
-                    </div>
-                    <button class="btn btn-primary w-100 py-2" type="submit">Entrar</button>
-                    <p class="mt-3 mb-0 text-center">
-                        <a href="#" id="showRegisterForm">Criar Conta</a>
-                    </p>
-                </form>
-            </div>
-            <div id="register-form" class="container-fluid" style="display: none;">
-                <form class="p-4 bg-white shadow-lg rounded form-container" method="post" action="{{route('auth.register')}}">
-                    @csrf
-                    <h1 class="h3 mb-3 fw-normal text-center">Criar Conta</h1>
-                    <p class="small text-secondary text-center">Preencha os campos abaixo para criar uma conta.</p>
-                    <div class="form-floating mb-3">
-                        <input type="text" class="form-control" id="registerName" name="name" placeholder="Name Person" required>
-                        <label for="registerName">Nome de Usuário</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="email" class="form-control" id="registerEmail" name="registerEmail" placeholder="name@example.com" required>
-                        <label for="registerEmail">E-mail</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="registerPassword" name="registerPassword" placeholder="Password" required>
-                        <label for="registerPassword">Senha</label>
-                    </div>
-                    <div class="form-floating mb-3">
-                        <input type="password" class="form-control" id="confirmPassword" name="confirmPassword" placeholder="Confirm Password" required>
-                        <label for="confirmPassword">Confirmação de Senha</label>
-                    </div>
-                    <button class="btn btn-primary w-100 py-2" type="submit">Criar Conta</button>
-                    <p class="mt-3 mb-0 text-center">
-                        <a href="#" id="showLoginForm">Voltar para Login</a>
-                    </p>
-                </form>
+<body class="uk-background-default text-primary">
+    <div class="uk-align-center uk-width-large">
+        <ul class="uk-tab-alt uk-margin-top">
+            <li id="login-btn-action"><a href="#">Entrar</a></li>
+            <li id="register-btn-action"><a href="#">Criar Conta</a></li>
+        </ul>
+    </div>
+    <form class="uk-width-large uk-align-center" id="login-form" method="post" action="{{route('auth.login')}}" style="display: none">
+        @csrf
+        <div class="uk-margin">
+            <label class="uk-form-label">E-mail</label>
+            <div class="uk-form-controls uk-margin-small">
+                <input placeholder="E-mail" name="email" id="email" type="email" class="uk-input"/>
             </div>
         </div>
-    </div>
-</div>
+        <div class="uk-margin">
+            <div class="uk-form-label">Senha</div>
+            <div class="uk-form-controls uk-margin-small uk-form-controls-text">
+                <input placeholder="Senha" name="password" id="password" type="password"  class="uk-input"/>
+            </div>
+        </div>
+        <div class="uk-alert uk-margin" uk-alert>
+            <a href class="uk-alert-close" uk-close></a>
+            <div class="uk-alert-title">Aviso!</div>
+            <div class="uk-alert-description uk-text-muted">
+                Suas informações estão não são armazenadas nem informadas a terceiros. O processamento de dados é totalmente seguro.
+            </div>
+        </div>
+        <button class="uk-button uk-button-primary uk-width-large">Entrar</button>
+    </form>
+    <form class="uk-width-large uk-align-center" id="register-form" method="post" action="{{route('auth.register')}}" style="display: none">
+        @csrf
+        <div class="uk-margin">
+            <label class="uk-form-label">Nome Completo</label>
+            <div class="uk-form-controls uk-margin-small">
+                <input placeholder="Nome" name="name" id="name" type="text" class="uk-input"/>
+            </div>
+        </div>
+        <div class="uk-margin">
+            <label class="uk-form-label">CPF</label>
+            <div class="uk-form-controls uk-margin-small">
+                <input placeholder="CPF" name="document" id="document" type="text" class="uk-input"/>
+            </div>
+        </div>
+        <div class="uk-margin">
+            <label class="uk-form-label">E-mail</label>
+            <div class="uk-form-controls uk-margin-small">
+                <input placeholder="E-mail" name="email" id="email" type="email" class="uk-input"/>
+            </div>
+        </div>
+        <div class="uk-margin">
+            <div class="uk-form-label">Senha</div>
+            <div class="uk-form-controls uk-margin-small uk-form-controls-text">
+                <input placeholder="Senha" name="password" id="password" type="password"  class="uk-input"/>
+            </div>
+        </div>
+        <div class="uk-alert uk-margin" uk-alert>
+            <a href class="uk-alert-close" uk-close></a>
+            <div class="uk-alert-title">Aviso!</div>
+            <div class="uk-alert-description uk-text-muted">
+                Suas informações estão não são armazenadas nem informadas a terceiros. O processamento de dados é totalmente seguro.
+            </div>
+        </div>
+        <button class="uk-button uk-button-primary uk-width-large">Criar Conta</button>
+    </form>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            document.getElementById('login-btn-action').addEventListener('click', function(e) {
+                e.preventDefault();
+                document.getElementById('login-btn-action').classList.add('uk-active');
+                document.getElementById('register-form').style.display = 'none';
+                document.getElementById('login-form').style.display = 'block';
+                document.getElementById('register-btn-action').classList.remove('uk-active');
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-<script>
-    document.addEventListener('DOMContentLoaded', function() {
-        document.getElementById('showRegisterForm').addEventListener('click', function(e) {
-            e.preventDefault();
-            document.getElementById('login-form').style.display = 'none';
-            document.getElementById('register-form').style.display = 'block';
+            });
+            document.getElementById('register-btn-action').addEventListener('click', function(e) {
+                e.preventDefault();
+                document.getElementById('register-btn-action').classList.toggle('uk-active');
+                document.getElementById('login-form').style.display = 'none';
+                document.getElementById('register-btn-action').classList.add('uk-active');
+                document.getElementById('register-form').style.display = 'block';
+                document.getElementById('login-btn-action').classList.remove('uk-active');
+
+            });
         });
 
-        document.getElementById('showLoginForm').addEventListener('click', function(e) {
-            e.preventDefault();
-            document.getElementById('register-form').style.display = 'none';
-            document.getElementById('login-form').style.display = 'block';
-        });
-    });
-</script>
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.21.5/dist/js/uikit.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/uikit@3.21.5/dist/js/uikit-icons.min.js"></script>
 </body>
 </html>
