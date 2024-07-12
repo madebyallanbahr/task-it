27f54f24762931e68f0693769d19c9a4c47f512c
Made by Allan Bahr <madebyallanbahr@gmail.com>
2024-07-12 00:47:09 -0300

    feat: working on in the dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -7,10 +7,19 @@
     <link rel="stylesheet" href="https://unpkg.com/franken-ui-releases@0.0.13/dist/default.min.css">
 </head>
 <body class="uk-background-default text-primary">
-    <p> Olá, seja bem vindo {{$user->name}}</p>
-    <form method="post" action="{{route('auth.logout')}}">
-        @csrf
-        <button class="uk-button uk-button-primary" type="submit">Sair</button>
-    </form>
+<div class="uk-container"> <!-- Container principal -->
+    <h2 class="uk-margin-top uk-position-left uk-margin-large-left">Seja bem vindo {{"@"}}{{$user->name}}</h2>
+    <div class="uk-position-top-right uk-margin-top uk-margin-large-right">
+        <form method="post" action="{{ route('auth.logout') }}">
+            @csrf
+            <button class="uk-button uk-button-ghost" type="submit"><span uk-icon="icon: sign-out; ratio: 1"></span></button>
+        </form>
+    </div>
+    <div>
+        <p>Hellow</p>
+    </div>
+</div>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.21.5/dist/js/uikit.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/uikit@3.21.5/dist/js/uikit-icons.min.js"></script>
 </body>
 </html>
